c27b6a550db5661f0a274054ab591bb814ae10ff
Gábor Kovács <gkovacs81@gmail.com>
2018-12-31 15:48:51 +0100

    Fix chrome config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     // custom flag for running build on travis
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
