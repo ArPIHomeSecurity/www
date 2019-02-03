3e1a25b88019a749f13e4028edef583b6b0556cf
gkovacs81 <gkovacs81@gmail.com>
2019-02-03 22:01:02 +0100

    FormBucket => Formspree

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-import { ReCaptcha2Component } from 'ngx-captcha';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -12,37 +12,15 @@ import { ReCaptcha2Component } from 'ngx-captcha';
 export class ContactComponent implements OnInit {
 
   public contactForm: FormGroup;
-  @ViewChild('captchaElem') captchaElem: ReCaptcha2Component;
-
-  // recaptcha site key
-  siteKey = '6LeEc4UUAAAAAIRxla7U89i8M4FepVZnc4oxEb0Q';
-
-  // reCaptcha result
-  isHuman = false;
+  email = "Z2tvdmFjczgxQGdtYWlsLmNvbQ==";
 
   constructor(
     private formBuilder: FormBuilder
-  ) { }
+  ) {
+    this.email  = atob(this.email);
+  }
 
   ngOnInit() {
-    this.contactForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      subject: ['', Validators.required],
-      message: ['', Validators.required],
-      recaptcha: ['', Validators.required]
-    });
-  }
 
-  invalidated(): void {
-    this.isHuman = false;
-  }
-
-  validated(captchaResponse: string): void {
-    this.isHuman = true;
-  }
-
-  submit(event) {
-    event.target.submit();
-    this.captchaElem.resetCaptcha();
   }
 }
