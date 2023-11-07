3223bec5dff21d94dd7e0d8c6f02b920acbe307f
gkovacs81 <gkovacs81@gmail.com>
2023-11-07 15:15:40 +0100

    V2.0 product images

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -11,21 +11,33 @@ export class PreviewComponent implements OnInit {
 
   images_0_8: Image[] = [
     new Image(0, {
-      img: 'assets/ArPIV0.8/IMG_1215.JPG'
+      img: 'assets/ArPI_V0.8/IMG_1215.JPG'
     }),
     new Image(1, {
-      img: 'assets/ArPIV0.8/IMG_1213.JPG'
+      img: 'assets/ArPI_V0.8/IMG_1213.JPG'
     }),
     new Image(2, {
-      img: 'assets/ArPIV0.8/IMG_1214.JPG'
+      img: 'assets/ArPI_V0.8/IMG_1214.JPG'
     })
   ];
   images_0_9: Image[] = [
     new Image(0, {
-      img: 'assets/ArPIV0.9/IMG_20220814_115448_376.jpg'
+      img: 'assets/ArPI_V0.9/IMG_20220814_115448_376.jpg'
     }),
     new Image(1, {
-      img: 'assets/ArPIV0.9/IMG_20220814_115529_488.jpg'
+      img: 'assets/ArPI_V0.9/IMG_20220814_115529_488.jpg'
+    })
+  ];
+
+  images_2_0: Image[] = [
+    new Image(0, {
+      img: 'assets/ArPI_V2.0/IMG_20231107_135118_600.jpg'
+    }),
+    new Image(1, {
+      img: 'assets/ArPI_V2.0/IMG_20231107_135201_384.jpg'
+    }),
+    new Image(2, {
+      img: 'assets/ArPI_V2.0/IMG_20231107_135226_259.jpg'
     })
   ];
 
